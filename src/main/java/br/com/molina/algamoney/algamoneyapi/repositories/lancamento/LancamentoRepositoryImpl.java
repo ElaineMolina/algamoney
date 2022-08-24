d6513881c8a73ec54afdfd89ba8de8a46d80407a
ElaineMolina <elainemolina10@gmail.com>
2022-08-24 16:21:25 -0300

    5.9 Implementando a paginação de lançamentos

--- src/main/java/br/com/molina/algamoney/algamoneyapi/repositories/lancamento/LancamentoRepositoryImpl.java
+++ src/main/java/br/com/molina/algamoney/algamoneyapi/repositories/lancamento/LancamentoRepositoryImpl.java
@@ -3,6 +3,9 @@ package br.com.molina.algamoney.algamoneyapi.repositories.lancamento;
 import br.com.molina.algamoney.algamoneyapi.domain.models.Lancamento;
 import br.com.molina.algamoney.algamoneyapi.domain.models.Lancamento_;
 import br.com.molina.algamoney.algamoneyapi.repositories.filter.LancamentoFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.util.ObjectUtils;
 
 import javax.persistence.EntityManager;
@@ -21,7 +24,7 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
     private EntityManager manager;
 
     @Override
-    public List<Lancamento> filtrar(LancamentoFilter lancamentoFilter) {
+    public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable) {
         CriteriaBuilder builder = manager.getCriteriaBuilder();
         CriteriaQuery<Lancamento> criteria = builder.createQuery(Lancamento.class);
         Root<Lancamento> root = criteria.from(Lancamento.class);
@@ -30,8 +33,11 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
         Predicate[] predicates = criarRestricoes(lancamentoFilter, builder, root);
         criteria.where(predicates);
         TypedQuery<Lancamento> query = manager.createQuery(criteria);
-        return query.getResultList();
+
+        adicionarRestriçõesDePaginacao(query, pageable);
+        return new PageImpl<>(query.getResultList(), pageable, total(lancamentoFilter));
     }
+
 
     private Predicate[] criarRestricoes(LancamentoFilter lancamentoFilter, CriteriaBuilder builder,
                                         Root<Lancamento> root) {
@@ -52,5 +58,25 @@ public class LancamentoRepositoryImpl implements LancamentoRepositoryQuery {
                     builder.lessThanOrEqualTo(root.get(Lancamento_.dataVencimento), lancamentoFilter.getDataVencimentoAte()));
         }
         return predicates.toArray(new Predicate[predicates.size()]);
+    }
+    private void adicionarRestriçõesDePaginacao(TypedQuery<Lancamento> query, Pageable pageable) {
+        int paginaAtual = pageable.getPageNumber();
+        int totalRegistrosPorPagina = pageable.getPageSize();
+        int primeiroRegistroDaPagina = paginaAtual * totalRegistrosPorPagina;
+
+        query.setFirstResult((primeiroRegistroDaPagina));
+        query.setMaxResults(totalRegistrosPorPagina);
+    }
+    private Long total(LancamentoFilter lancamentoFilter) {
+        CriteriaBuilder builder = manager.getCriteriaBuilder();
+        CriteriaQuery<Long> criteria = builder.createQuery(Long.class);
+        Root<Lancamento> root = criteria.from(Lancamento.class);
+
+        Predicate[] predicates = criarRestricoes(lancamentoFilter, builder, root);
+        criteria.where(predicates);
+
+        criteria.select(builder.count(root));
+
+        return manager.createQuery(criteria).getSingleResult();
     }
 }
--- src/main/java/br/com/molina/algamoney/algamoneyapi/repositories/lancamento/LancamentoRepositoryQuery.java
+++ src/main/java/br/com/molina/algamoney/algamoneyapi/repositories/lancamento/LancamentoRepositoryQuery.java
@@ -2,11 +2,13 @@ package br.com.molina.algamoney.algamoneyapi.repositories.lancamento;
 
 import br.com.molina.algamoney.algamoneyapi.domain.models.Lancamento;
 import br.com.molina.algamoney.algamoneyapi.repositories.filter.LancamentoFilter;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface LancamentoRepositoryQuery  {
 
-    public List< Lancamento> filtrar(LancamentoFilter lancamentoFilter);
+    public Page<Lancamento> filtrar(LancamentoFilter lancamentoFilter, Pageable pageable);
 }
